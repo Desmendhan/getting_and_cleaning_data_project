4bf398dfc4f959b850a8b998a8954bf9a7f4de3b
Greg Sutcliffe <greg.sutcliffe@gmail.com>
2018-01-14 00:24:59 +0000

    initial commit - week 4 course project

---
+++ README.md
@@ -0,0 +1,89 @@
+# Getting and Clean Data - Course Project
+
+This repo contains the analysis script and codebook for the week 4 course
+project of Getting and Cleaning Data. This file explains the workings of the
+script.
+
+# Running the script
+
+You will need the Samsung UCI HAR data unzipped in this directory - i.e the
+directory `UCI HAR Dataset` needs to exist. The script will exit with an error
+if it cannot find the data.
+
+You will also need the `dplyr` package available. The script will load it
+automatically.
+
+Once these conditions are met, simply `source('run_analysis.R')` to run the
+script and return the analysed tidy data set.
+
+# How the script works
+
+The script is split into 5 functions, each of which performs a specific goal.
+Each function except the first takes a single `data.frame` argument, so
+chaining can be used between the functions. At the end of the script we call
+all 5 functions in sequence (using chaining) to get the final dataset.
+
+## Function 1 - loading the data from files
+
+The first function handles all the file loading, and creates the initial
+*merged* messy dataset. It loads all the data in:
+
+* features.txt
+* X_train.txt
+* X_test.txt
+* Y_train.txt
+* Y_test.txt
+* subject_train.txt
+* subject_test.txt
+
+It first merges the `X_train.txt` and `X_test.txt` files, and uses the data in
+`features.txt` to set the column names. `rbind` is used to merge these into a
+single data.frame.
+
+Since the subject and activity IDs are also needed, it loads these from
+`subject_train.txt`, `subject_test.txt`, `Y_train.txt`, and `Y_test.txt`, again
+merging them with `rbind` and then adding them to the data.frame with `cbind`.
+Subject ID is also converted to a factor since it represents individuals.
+
+Finally, the messy dataset is returned from the function
+
+## Function 2 - selecting columns
+
+The assignment calls for all the means and standard deviations. The codebook
+shows that these columns are called `mean` and `std` so the script uses
+`select()` and `contains()` to extract all the matching columns, as well as the
+`subject` and `activity` columns needed for later grouping.
+
+However, this also matches `meanFreq` and 7 columns starting with `angle` which
+are not required, so a second `select` is called to remove these columns. The
+extracted dataset is then returned from the function.
+
+## Function 3 - labelling activities
+
+At this point the dataset has activities as a numeric ID, which needs to be
+corrected.
+
+The script first loads the activity labels from `activity_labels.txt` and
+merges this into the main dataset. This produces a new column `label` *in
+addition to* the `activity` numeric ID, so a `mutate` and `select` is used to
+update the `activity` column with the text, and remove the unnecessary `label`
+column. The updated dataset is then returned.
+
+## Function 4 - tidying variable names
+
+This function simply manipulates the column names to make them a bit more readable.
+
+First, note that names like `tBodyGyro.std` don't look good with a simple
+lowercase (e.g. `tbodygyro.std`) so instead we use a series of `gsub` sunctions
+to replace capitalised words with lowercase & an underscore. Then extra periods
+are cleaned up, and a final `tolower` used to ensure nothing was missed.
+
+Finally, the new column names are assigned to the dataset and the dataset is
+returned.
+
+## Function 5 - calculating means per subject & activity
+
+`Dplyr` makes this stage very easy. First, `group_by` will deal with the
+grouping, after which `summarise_all` will calculate the mean for every
+ungrouped column. Finally `arrange` is used to sort the result nicely, and the
+result is returned for printing.
---
+++ run_analysis.R
@@ -0,0 +1,122 @@
+# G S Sutcliffe - Jan 2018
+
+# Script to perform the required analysis for the final project
+# in Coursera's Geting and Cleaning Data course
+
+# This script comprises 5 functions which correspond to the steps in the
+# project outline. Each function is commented for clarity.
+
+# load libraries
+library(dplyr)
+
+# The script assumes that the HAR dataset is in the current working directory,
+# so we first check it's presence and exit if not found
+if (!file.exists('./UCI HAR Dataset/')) {stop('UCI HAR Dataset directory not found')}
+
+merge_raw_data <- function() {
+    # This function handles loading all the required files and setting the column
+    # names - the raw measurements, the activity IDs, and the subject IDs. It
+    # returns the merged dataset as a dplyr data.frame
+
+    # Since read.table can set the column names as it's loading the data, we'll
+    # read features.txt first and use it to set the column names, rather than
+    # adding them later via names()
+
+    # load the column names from features.txt
+    features <- read.table('UCI HAR Dataset/features.txt',col.names = c('id','feature'))
+
+    # read the raw data, setting the column names as well, and merge row-wise
+    test_data  <- tbl_df(read.table('UCI HAR Dataset/test/X_test.txt',col.names = (features$feature)))
+    train_data <- tbl_df(read.table('UCI HAR Dataset/train/X_train.txt',col.names = (features$feature)))
+    raw <- rbind(train_data,test_data)
+
+    # The activity ID is needed for later grouping, so read it, merge it row-wise
+    # and add it as a column to 'raw'
+    activity_test  <- read.table('./UCI HAR Dataset/test/y_test.txt',col.names = c('activity'))
+    activity_train <- read.table('./UCI HAR Dataset/train/y_train.txt',col.names = c('activity'))
+    activities     <- rbind(activity_train,activity_test)
+    raw            <- cbind(raw,activities)
+
+    # The subject ID is also needed later for grouping, so again, read it, merge
+    # it, and add it as a column
+    subject_test  <- read.table('./UCI HAR Dataset/test/subject_test.txt',col.names = c('subject'))
+    subject_train <- read.table('./UCI HAR Dataset/train/subject_train.txt',col.names = c('subject'))
+    subjects      <- rbind(subject_train,subject_test)
+    raw           <- cbind(raw,subjects)
+
+    # Convert the subject ID to a factor while we're here, since each ID is a
+    # separate individual
+    mutate(raw, subject = as.factor(subject))
+
+}
+
+extract_means_and_stds <- function(dataset) {
+    # This function filters for the columns we're looking for, which is anything
+    # that is a mean or standard deviation
+
+    # Since we're using Dplyr, we can use contains() in select() to do this
+    # easily. We also need to keep the activity and subject ID columns.
+    select(dataset,subject,activity,contains('mean'),contains('std')) %>%
+        # We don't, however, want the 'meanFreq' or 'angle.*' variables
+        select(-contains('meanFreq'),-starts_with('angle'))
+}
+
+label_activities <- function(dataset) {
+    # This function reads the activity labels from activity_labels.txt and
+    # converts the activity column to a factor using this data
+
+    labels <- read.table('./UCI HAR Dataset/activity_labels.txt',col.names = c('activity','label'))
+
+    # Now merge the data and select the label, and use that to replace the ID
+    # column in the original dataset.
+    merge(dataset,labels) %>%
+        mutate(activity = label) %>%
+        select(-label)
+}
+
+rename_variables <- function(dataset) {
+    #This function just does a bit of tidying up on the variable names
+
+    names <- names(dataset)
+
+    # Replace CamelCase variables with lowercase + underscore
+    names <- gsub('Acc','_acc',names)
+    names <- gsub('Body','_body',names)
+    names <- gsub('Gravity','_gravity',names)
+    names <- gsub('Gyro','_gyro',names)
+    names <- gsub('Jerk','_jerk',names)
+    names <- gsub('Mag','_mag',names)
+
+    # Remove trailing '..' from line ends
+    names <- sub('\\.\\.$','',names)
+
+    # Replace '...' with just '.'
+    names <- gsub('\\.\\.\\.','\\.',names)
+
+    # Lowercase the XYZ variables
+    names <- tolower(names)
+
+    # Assign the new names and return it
+    names(dataset) <- names
+    dataset
+}
+
+summarise_means_stds <- function(dataset) {
+    # Thanks to dplyr, this is easy. We group by subject and activity, and
+    # then call summarise_all to return the mean for every remaining column
+
+    dataset %>%
+        group_by(subject,activity) %>%
+        summarise_all(mean) %>%
+        arrange(subject,activity)
+}
+
+# Having defined all the functions, we now call them in order to produce the
+# final dataset. We return two variables - the tidy form of the raw data, and
+# grouped means per subject & activity
+
+merge_raw_data() %>%
+    extract_means_and_stds %>%
+    label_activities %>%
+    rename_variables %>%
+    summarise_means_stds
